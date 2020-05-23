036dd0fd187b4ecb5f837c23ba42a75c0aefc804
Andi Irham <Andi.irhamm@gmail.com>
2020-05-23 16:49:11 +0700

    Updates

---
+++ precache-manifest.1b61cdc0861437b24c56786f985abb9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c357903cc2f98c7ec5aea3f032bf0b4",
+    "url": "/react-porto/index.html"
+  },
+  {
+    "revision": "8cb85f9c3b65d55fce09",
+    "url": "/react-porto/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "863a59ede416ea21c22c",
+    "url": "/react-porto/static/js/2.770573d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-porto/static/js/2.770573d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cb85f9c3b65d55fce09",
+    "url": "/react-porto/static/js/main.be2e6d49.chunk.js"
+  },
+  {
+    "revision": "4633deb958852fd51552",
+    "url": "/react-porto/static/js/runtime-main.827350a4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-porto/static/media/logo.5d5d9eef.svg"
+  }
+]);
